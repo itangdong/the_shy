cbee77185f08c65146589afe7c000d08b2a8a024
itangdong <itangdong@163.com>
2019-05-21 21:44:12 +0800

    test

--- src/router.js
+++ src/router.js
@@ -3,12 +3,13 @@ import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 import Products from './routes/Products';
 import autoForm from './routes/AutoForm';
+import testPrint from './routes/testPrint';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={testPrint} />
         <Route path="/products" exact component={Products} />
         <Route path="/autoform" exact component={autoForm} />
       </Switch>
